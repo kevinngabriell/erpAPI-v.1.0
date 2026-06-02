74c6dc70452aedd843767bf6d15b47b17c3c0204
kevinngabriell <kevingabriel786@gmail.com>
2026-06-02 12:38:45 +0700

    cors

--- master/finance/account-code.php
+++ master/finance/account-code.php
@@ -5,8 +5,11 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    http_response_code(200);
+    exit();
 }
 
 require_once '../../general.php';
