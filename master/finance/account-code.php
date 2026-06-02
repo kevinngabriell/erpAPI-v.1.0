64fcc00ccdfd5a539d3ac0d59017d19b293ee0cb
kevinngabriell <kevingabriel786@gmail.com>
2026-06-02 12:31:00 +0700

    cors added

--- master/finance/account-code.php
+++ master/finance/account-code.php
@@ -1,5 +1,14 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 require_once '../../general.php';
 require_once '../../vendor/autoload.php';
 require_once '../../connection/connection.php';
