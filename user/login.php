02c442b66db47ceedba015dea869ee2f6fcae6c3
kevinngabriell <kevingabriel786@gmail.com>
2026-06-04 13:23:54 +0700

    test cors

--- user/login.php
+++ user/login.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
